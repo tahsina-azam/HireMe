aaa133d8afda86ffd61cf7dc0f1cfca936241c74
Tahsina Bintay Azam <52793150+tahsina-azam@users.noreply.github.com>
2022-07-11 03:28:26 +0600

    added post body for dashboard

---
+++ pages/worker/works.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import PostCard from '../../src/components/post/postcard'
+
+function works() {
+  return (
+    <div>
+        <PostCard/>
+    </div>
+  )
+}
+
+export default works
