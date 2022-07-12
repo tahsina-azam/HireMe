e1ba098459e5e98fa0b49a3e8ac4b24765176e51
Tahsina Bintay Azam <52793150+tahsina-azam@users.noreply.github.com>
2022-07-12 12:23:01 +0600

    user profile

---
+++ pages/worker/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import WorkerProfile from '../../src/components/workerProfile'
+
+function index() {
+  return (
+    <div><WorkerProfile/></div>
+  )
+}
+
+export default index
---
+++ src/components/workerProfile.js
@@ -0,0 +1,81 @@
+import React from 'react'
+import { Stack } from '@mui/material'
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import ListSubheader from '@mui/material/ListSubheader';
+import { Box } from '@mui/system';
+
+function workerProfile() {
+  return (
+    <div>
+          <Box
+        sx={{
+          borderColor: 'primary.main',
+          marginTop: 10,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Stack direction="row" spacing={10}>
+        <Card sx={{ maxWidth: 345 ,minWidth: 300 }}>
+      <CardMedia
+        component="img"
+        alt="green iguana"
+        height="140"
+        image="/static/images/cards/contemplative-reptile.jpg"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Lizard
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          Lizards are a widespread group of squamate reptiles, with over 6,000
+          species, ranging across all continents except Antarctica
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Share</Button>
+        <Button size="small">Learn More</Button>
+      </CardActions>
+    </Card>
+    <List
+      sx={{
+        width: '150%',
+        maxWidth: 360,
+        minWidth:300,
+        bgcolor: 'background.paper',
+        position: 'relative',
+        overflow: 'auto',
+        maxHeight: 300,
+        '& ul': { padding: 0 },
+      }}
+      subheader={<li />}
+    >
+      {[0, 1, 2, 3, 4].map((sectionId) => (
+        <li key={`section-${sectionId}`}>
+          <ul>
+            <ListSubheader>{`I'm sticky ${sectionId}`}</ListSubheader>
+            {[0, 1, 2].map((item) => (
+              <ListItem key={`item-${sectionId}-${item}`}>
+                <ListItemText primary={`Item ${item}`} />
+              </ListItem>
+            ))}
+          </ul>
+        </li>
+      ))}
+    </List>
+        </Stack>
+        </Box>
+    </div>
+  )
+}
+
+export default workerProfile
